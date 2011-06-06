ac1f24663e89c71132d49d41ec0f92887e76e25f
jmt <jeanmarie.traissard@prowebce.com>
2011-06-06 16:34:57 +0200

    ADD: module accueil

---
+++ apps/frontend/modules/accueil/actions/actions.class.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * accueil actions.
+ *
+ * @package    quotes
+ * @subpackage accueil
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class accueilActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->quotes=Doctrine_Core::getTable('Quote')->findValidQuotes()->orderBy('updated_at')
+    ->execute();
+  }
+}
---
+++ apps/frontend/modules/accueil/templates/indexSuccess.php
@@ -0,0 +1,4 @@
+<?php foreach($quotes as $quote): ?>
+  <?php include_partial('quote/quote',array('date' => $quote->getCreatedAt(),'auteur' => $quote->getAuteur(),'on_dit' => $quote->getOnDit(),'on_dit_pas' => $quote->getOnDitPas())) ?>
+  <hr/>
+<?php endforeach ?>
